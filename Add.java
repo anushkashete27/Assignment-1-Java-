e1da8f7e02644c9897d84ec104a27b5378f72b5e
anushkashete27 <anushkashete272007@gmail.com>
2025-07-24 16:41:20 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,14 @@
+public class Add
+{
+public static void main(String arg[])
+{
+int a=2;
+int b=4;
+int c=5;
+int d=8;
+int e=6;
+
+System.out.println(a+b+c+d+e);
+System.out.println(a*b*c*d*e);
+}
+}
